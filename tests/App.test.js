c052c76c1baf8df0109b285b8ab8e21ac27f3a59
205-sys <bunsengsan205@gmail.com>
2026-06-25 15:13:00 +0700

    Update App.test.js

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,8 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import App from "./App";
-
+import App from "../src/App";
 describe("App", () => {
   it("renders the landing page", () => {
     render(<App />);
