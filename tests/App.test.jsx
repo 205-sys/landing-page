6a3eae91b0dea0859a6fc777e952256bd88500f3
205-sys <bunsengsan205@gmail.com>
2026-06-25 15:21:25 +0700

    Update App.test.jsx

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/vitest";
 import App from "../src/App.jsx";
 
 describe("App", () => {
